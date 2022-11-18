cdad23d9d71f7fd81ddd7ecc826c41b52fb470ba
weiss <kingbaiing@163.com>
2022-11-18 20:03:40 +0100

    unset i in org-noter

--- emacs-config/config/weiss_org-noter_keybindings.el
+++ emacs-config/config/weiss_org-noter_keybindings.el
@@ -1,4 +1,5 @@
 (with-eval-after-load 'org-noter
+  (wks-unset-key org-noter-doc-mode-map '("i"))
   (define-key org-noter-notes-mode-map [remap xref-find-definitions] #'org-noter-sync-current-note)
   (wks-define-key
    org-noter-doc-mode-map
