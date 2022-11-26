cf98aaf5bfb34a19ad67deb5bc792b9ebe4e1ef1
weiss <kingbaiing@163.com>
2022-11-26 08:54:59 +0100

    update org-noter location setting

--- emacs-config/config/weiss_org-noter_settings.el
+++ emacs-config/config/weiss_org-noter_settings.el
@@ -1,5 +1,12 @@
 (with-eval-after-load 'org-noter
-
-)
+  (setq org-noter-notes-window-location
+        (pcase emacs-host
+          ("arch"
+           'vertical-split 
+           )
+          ("mac"
+           'horizontal-split
+           )))
+  )
 
 (provide 'weiss_org-noter_settings)
