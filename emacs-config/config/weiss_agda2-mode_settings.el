f2bcbbc7be0ff25376fd2b7ecb2d490248da6af6
weiss <kingbaiing@163.com>
2022-11-30 21:23:35 +0100

    add agda settings

---
+++ emacs-config/config/weiss_agda2-mode_settings.el
@@ -0,0 +1,7 @@
+(with-eval-after-load 'agda2-mode
+  (setq-mode-local agda2-mode completion-ignore-case t)
+  (setq agda-input-user-translations '(("++" "⧺") ("<=" "≼") ))
+  ;; update settings
+  (agda-input-setup)
+  )
+(provide 'weiss_agda2-mode_settings)
