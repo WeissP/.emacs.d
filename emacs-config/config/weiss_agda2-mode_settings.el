804fd8b75ec09e4a4973a6dd2a9df7333431de59
weiss <kingbaiing@163.com>
2022-12-11 22:51:21 +0100

    try activating agda input method at start

--- emacs-config/config/weiss_agda2-mode_settings.el
+++ emacs-config/config/weiss_agda2-mode_settings.el
@@ -1,4 +1,5 @@
 (with-eval-after-load 'agda2-mode
+  (require 'agda-input)
   (setq-mode-local agda2-mode completion-ignore-case t)
   (setq agda-input-user-translations '(("++" "⧺") ("<=" "≼") ))
   ;; update settings
