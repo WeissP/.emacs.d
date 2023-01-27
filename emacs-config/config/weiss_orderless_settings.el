7fdc1ab5ace014a1915e3c99553317cd66ecc8c4
weiss <kingbaiing@163.com>
2023-01-27 20:28:57 +0100

    let orderless support search space

--- emacs-config/config/weiss_orderless_settings.el
+++ emacs-config/config/weiss_orderless_settings.el
@@ -1,6 +1,8 @@
 (with-eval-after-load 'orderless
-  (setq completion-styles '(orderless))
-)
+  (setq orderless-component-separator 'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-overrides '((file (styles basic partial-completion))))
+  )
 
 ;; parent: 
 (provide 'weiss_orderless_settings)
